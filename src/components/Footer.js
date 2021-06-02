881dbbe1d9bc16ee87fbeb2214acddd397878295
Rafael Schiefelbein <80975453+Rafa3lSch@users.noreply.github.com>
2021-06-02 14:52:23 +0200

    initial

---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <span>
+      Made with{" "}
+      <a href="https://fr.reactjs.org/" target="_blank" rel="noreferrer">
+        React
+      </a>{" "}
+      by{" "}
+      <a
+        href="https://www.linkedin.com/in/rafael-schiefelbein-51127495/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Rafael Sch
+      </a>
+    </span>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <FontAwesomeIcon className="space-shuttle" icon="space-shuttle" />
+      Ready To Go
+    </div>
+  );
+};
+
+export default Header;
